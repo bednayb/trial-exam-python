da9472a2b88ca84a94c6e581dcd1d89003b157ea
bednayb <bednaybence@gmail.com>
2016-11-30 07:58:10 +0100

    3 is ready

--- 1.py
+++ 1.py
@@ -1,3 +1,16 @@
 # Create a function that takes a list as a parameter,
 # and returns a new list with all its element's values doubled.
 # It should raise an error if the parameter is not a list.
+
+### 3. exercise left ###
+
+### change print to return ###
+
+list_for_function =[2,3,4,5,10]
+
+def list_doubled(list_simple):
+    for i in range(len(list_simple)):
+        list_simple[i] *= 2
+    print(list_simple)
+
+list_doubled(list_for_function)
--- 3.py
+++ 3.py
@@ -2,3 +2,24 @@
 # It should take its three sizes as constructor parameters (numbers)
 # It should have a method called `getSurface` that returns the cuboid's surface
 # It should have a method called `getVolume` that returns the cuboid's volume
+
+
+
+class Cuboid():
+
+    def __init__(self,a,b,c):
+        self.a = a
+        self.b = b
+        self.c = c
+
+
+    def getSurface(self):
+        return self.a*self.b*2 + self.a*self.c*2 + self.b*self.c*2
+
+    def getVolume(self):
+        return self.a*self.b*self.c
+
+x =Cuboid(4,5,6)
+
+x.getSurface()
+x.getVolume()
