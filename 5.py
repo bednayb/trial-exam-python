0b0df14a667429aead9241a4602439fbee43fe08
bednayb <bednaybence@gmail.com>
2016-11-30 10:06:09 +0100

    ready

--- 5.py
+++ 5.py
@@ -1,11 +1,21 @@
 pirates = [
-  {'Name': 'Olaf', 'has_wooden_leg': False, 'gold': 12},        
-  {'Name': 'Uwe', 'has_wooden_leg': True, 'gold': 9},        
-  {'Name': 'Jack', 'has_wooden_leg': True, 'gold': 16},        
-  {'Name': 'Morgan', 'has_wooden_leg': False, 'gold': 17},        
-  {'Name': 'Hook', 'has_wooden_leg': True, 'gold': 20},        
+  {'Name': 'Olaf', 'has_wooden_leg': False, 'gold': 12},
+  {'Name': 'Uwe', 'has_wooden_leg': True, 'gold': 9},
+  {'Name': 'Jack', 'has_wooden_leg': True, 'gold': 16},
+  {'Name': 'Morgan', 'has_wooden_leg': False, 'gold': 17},
+  {'Name': 'Hook', 'has_wooden_leg': True, 'gold': 20},
 ]
 
 # Write a function that takes any list that contains pirates as in the example,
 # And returns a list of names containing the pirates that has wooden leg and
 # more than 15 gold
+
+def pirates_check(check_dictonary):
+    rich_pirates_with_wood_leg = []
+    for i in range(len(check_dictonary)):
+        if check_dictonary[i]["has_wooden_leg"] == True and check_dictonary[i]["gold"] > 15:
+            rich_pirates_with_wood_leg.append(check_dictonary[i]["Name"])
+
+    return rich_pirates_with_wood_leg
+
+pirates_check(pirates)
